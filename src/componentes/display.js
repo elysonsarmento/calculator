f50571588415693537cd5ec2784ee4cc06713bd7
Elyson Sarmento <55628855+elysonsarmento@users.noreply.github.com>
2020-10-06 23:47:31 -0300

    first commit

---
+++ src/componentes/display.js
@@ -0,0 +1,24 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react'
+
+const style = StyleSheet.create({
+    display:{
+        flex: 1,
+        padding: 12,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        alignItems: 'flex-end'
+    },
+    displayValue:{
+        fontSize: 60,
+        color: '#fff',
+
+    }
+    
+})  
+
+export default props =>
+<View style={style.display}>
+    <Text style={style.displayValue}
+    numberOfLines={1}>{props.value}</Text>
+</View>
